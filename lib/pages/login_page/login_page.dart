568424395aee2188adaf58394618480aab2b5859
nshelestoff <69767772+nshelestoff@users.noreply.github.com>
2021-08-04 22:39:26 +0300

    started working at login pages implementation

---
+++ lib/pages/login_page/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Text('Hello world!'),
+      ),
+    );
+  }
+}
