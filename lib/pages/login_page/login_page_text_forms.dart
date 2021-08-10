67f6595d117e80e355d997526e245a2e5b6fab02
nshelestoff <69767772+nshelestoff@users.noreply.github.com>
2021-08-10 18:11:01 +0300

    added login page missed functional

--- lib/pages/login_page/login_page_text_forms.dart
+++ lib/pages/login_page/login_page_text_forms.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:crypto_signal_app/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:crypto_signal_app/pages/login_page/sign_in_page.dart';
 import 'package:crypto_signal_app/pages/login_page/create_acc_page.dart';
 
@@ -23,7 +24,7 @@ class UserAcc {
 Widget buildFirstName() {
   return TextFormField(
     decoration: InputDecoration(
-      contentPadding: EdgeInsets.symmetric(vertical: 21.w, horizontal: 21.w),
+      contentPadding: EdgeInsets.symmetric(vertical: 21.h, horizontal: 21.w),
       hintText: 'First Name',
     ),
     validator: (String? value) {
@@ -39,7 +40,7 @@ Widget buildLastName() {
   return TextFormField(
     decoration: InputDecoration(
       hintText: 'Last Name',
-      contentPadding: EdgeInsets.symmetric(vertical: 21.w, horizontal: 21.w),
+      contentPadding: EdgeInsets.symmetric(vertical: 21.h, horizontal: 21.w),
     ),
     validator: (String? value) {
       if (value == null || value.isEmpty) {
@@ -54,7 +55,7 @@ Widget buildEmail() {
   return TextFormField(
     decoration: InputDecoration(
       hintText: 'Email',
-      contentPadding: EdgeInsets.symmetric(vertical: 21.w, horizontal: 21.w),
+      contentPadding: EdgeInsets.symmetric(vertical: 21.h, horizontal: 21.w),
     ),
     validator: (String? value) {
       if (value == null || value.isEmpty) {
@@ -73,7 +74,7 @@ Widget buildPhoneNumber() {
     keyboardType: TextInputType.phone,
     decoration: InputDecoration(
       hintText: 'Phone',
-      contentPadding: EdgeInsets.symmetric(vertical: 21.w, horizontal: 21.w),
+      contentPadding: EdgeInsets.symmetric(vertical: 21.h, horizontal: 21.w),
     ),
     validator: (String? value) {
       if (value == null || value.isEmpty) {
@@ -88,7 +89,7 @@ Widget buildLogin() {
   return TextFormField(
     decoration: InputDecoration(
       hintText: 'Login',
-      contentPadding: EdgeInsets.symmetric(vertical: 21.w, horizontal: 21.w),
+      contentPadding: EdgeInsets.symmetric(vertical: 21.h, horizontal: 21.w),
     ),
     validator: (String? value) {
       if (value == null || value.isEmpty) {
@@ -99,19 +100,91 @@ Widget buildLogin() {
   );
 }
 
-Widget buildPassword() {
-  return TextFormField(
-    keyboardType: TextInputType.visiblePassword,
-    obscureText: true,
-    decoration: InputDecoration(
-      hintText: 'Password',
-      contentPadding: EdgeInsets.symmetric(vertical: 21.w, horizontal: 21.w),
+class buildPassword extends StatefulWidget {
+  const buildPassword({Key? key}) : super(key: key);
+
+  @override
+  _buildPasswordState createState() => _buildPasswordState();
+}
+
+class _buildPasswordState extends State<buildPassword> {
+  bool obscurePass = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.h,
+      child: Stack(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.visiblePassword,
+            obscureText: obscurePass,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              contentPadding: EdgeInsets.symmetric(vertical: 21.h, horizontal: 21.w),
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Password is Required';
+              }
+              return null;
+            },
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: CupertinoButton(
+              minSize: 24.r,
+              child: SvgPicture.asset('assets/passwordeye.svg'),
+              onPressed: () {
+                setState(() {
+                  if (obscurePass == true) {
+                    obscurePass = false;
+                  }
+                  else
+                    {
+                      obscurePass = true;
+                    }
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+Widget buildPassword1() {
+  bool obscurePass = true;
+  return Container(
+    height: 56.h,
+    child: Stack(
+      children: [
+        TextFormField(
+          keyboardType: TextInputType.visiblePassword,
+          obscureText: obscurePass,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            contentPadding: EdgeInsets.symmetric(vertical: 21.h, horizontal: 21.w),
+          ),
+          validator: (String? value) {
+            if (value == null || value.isEmpty) {
+              return 'Password is Required';
+            }
+            return null;
+          },
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: CupertinoButton(
+            minSize: 24.r,
+            child: SvgPicture.asset('assets/passwordeye.svg'),
+            onPressed: () {
+              obscurePass = false;
+            },
+          ),
+        ),
+      ],
     ),
-    validator: (String? value) {
-      if (value == null || value.isEmpty) {
-        return 'Password is Required';
-      }
-      return null;
-    },
   );
 }
